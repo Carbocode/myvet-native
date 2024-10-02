3536dab7b4cc145f7c75875a95d4b726e99b7b54
Carbocode <noreply@prisco.me>
2024-10-02 15:55:14 +0200

    Initial Commit

---
+++ myvet-native/myvet_nativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  myvet_nativeApp.swift
+//  myvet-native
+//
+//  Created by Ligmab Allz on 2024-10-02.
+//
+
+import SwiftUI
+
+@main
+struct myvet_nativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
